68d0ee0268ad56c8ed308c406ceeebea23df35bd
Schaffer <mr.giancarloschaffer@gmail.com>
2021-03-05 17:13:04 -0300

    Connect with firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase";
+import "./App.scss";
+
+// database.push().set({});
+
+// database.on(
+//   "value",
+//   function (snapshot) {
+//     const response = snapshot.val();
+//     database.off();
+//   },
+//   function (errorObject) {
+//     console.log("The read failed: " + errorObject.code);
+//   }
+// );
+const RESETER = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
+  const database = firebase.database().ref().child("jogo");
+  const [gameState, setGameState] = useState([]);
+  const [current, setCurrent] = useState(true);
+
+  const CURRENT_MAP = {
+    true: "X",
+    false: "O",
+  };
+
+  useEffect(() => {
+    database.on(
+      "value",
+      function (snapshot) {
+        const response = snapshot.val();
+        setGameState(response.gameState);
+        setCurrent(response.current);
+      },
+      function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+    // eslint-disable-next-line
+  }, []);
+
+  const handleClick = (index) => {
+    const AuxState = [...gameState];
+    AuxState[index] = CURRENT_MAP[current];
+    database.update({ current: !current, gameState: AuxState });
+  };
+
+  const handleReset = () => {
+    database.update({ gameState: RESETER, current: true });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Current: {CURRENT_MAP[current]}</p>
+        <div className="App-board">
+          {gameState.map((e, i) => (
+            <span onClick={() => handleClick(i)} className="App-vals">
+              {e}
+            </span>
+          ))}
+        </div>
+        <span className="App-button" onClick={handleReset}>
+          Reset
+        </span>
       </header>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIX6dZfWq-uF8XZogwGUJBQogH30yAIkU",
+  authDomain: "xoxo-e7581.firebaseapp.com",
+  databaseURL: "https://xoxo-e7581-default-rtdb.firebaseio.com",
+  projectId: "xoxo-e7581",
+  storageBucket: "xoxo-e7581.appspot.com",
+  messagingSenderId: "306692024346",
+  appId: "1:306692024346:web:c8eaf53fee8bcb56a8c9a3",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
